cf05bb951d7b8a7b70c233f6f8c4f3d037e7554c
ehabhamdy <ehabhamdy2012@gmail.com>
2016-06-16 13:16:56 +0200

    Creating the dialog_add.xml

--- app/src/main/java/com/ehab/bucketdrop/ActivityMain.java
+++ app/src/main/java/com/ehab/bucketdrop/ActivityMain.java
@@ -3,21 +3,27 @@ package com.ehab.bucketdrop;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener {
 
 
     Toolbar mToolbar;
+    Button mAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        mAddButton = (Button) findViewById(R.id.btn_add);
+        mAddButton.setOnClickListener(this);
 
         setSupportActionBar(mToolbar);
 
@@ -30,5 +36,10 @@ public class ActivityMain extends AppCompatActivity {
                 .load(R.drawable.background)
                 .centerCrop()
                 .into(background);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
     }
 }
